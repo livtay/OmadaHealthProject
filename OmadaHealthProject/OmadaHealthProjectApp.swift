fb6acda67949663cf759925a5ef464675703adc0
Olivia Kline <oliviantaylor@gmail.com>
2023-08-22 09:26:09 -0700

    Initial Commit

---
+++ OmadaHealthProject/OmadaHealthProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmadaHealthProjectApp.swift
+//  OmadaHealthProject
+//
+//  Created by Olivia Taylor on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct OmadaHealthProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
